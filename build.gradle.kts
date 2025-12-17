6c4fadafdac7c98310e761a1e580ab73d6109f63
Jake Pronger <jakepronger31@gmail.com>
2025-12-17 18:20:40 +1100

    1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     `java-library`
-    id("maven-publish")
+    `maven-publish`
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 group = "me.jakepronger.spine"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
@@ -23,4 +26,24 @@ dependencies {
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+}
+
+// Modern Gradle 9 approach to configure the task
+tasks.named<ShadowJar>("shadowJar") {
+    archiveClassifier.set("")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("maven") {
+                // Now that the plugin is 'com.gradleup.shadow', this component should be found
+                from(components["shadow"])
+
+                groupId = "com.github.jakepronger"
+                artifactId = "Spine"
+                version = "1.0.1"
+            }
+        }
+    }
 }
